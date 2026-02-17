44191ba7405adc9a60db7a33940f5cf9db9362be
sarathi978652 <sarathi978652@gmail.com>
2026-02-17 14:16:12 +0530

    Add

---
+++ OOPSBanner1.java
@@ -0,0 +1,12 @@
+public class OOPSBanner1{
+	public static void main(String[] args){
+System.out.println(String.join(" ","   ***   ","   ***   ","******  ","  *****  "));
+System.out.println(String.join(" "," **   ** "," **   ** ","**   ** "," **    **"));
+System.out.println(String.join(" ","**     **","**     **","**    **"," **      "));
+System.out.println(String.join(" ","**     **","**     **","**   ** ","   ***** "));
+System.out.println(String.join(" ","**     **","**     **","******  ","       **"));
+System.out.println(String.join(" "," **   ** "," **   ** ","**      "," **    **"));
+System.out.println(String.join(" ","   ***   ","   ***   ","**      ","  ****** "));
+
+	}
+}
