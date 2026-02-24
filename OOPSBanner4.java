6992aadb02eabf29dbe3788c54592e6421c61943
sarathi978652 <sarathi978652@gmail.com>
2026-02-24 14:06:10 +0530

    UC4

---
+++ OOPSBanner4.java
@@ -0,0 +1,13 @@
+public class OOPSBanner4 {
+    public static void main(String[] args) {
+        String[]oPattern=getOPattern();
+		String[]pPattern=getPPattern();
+		String[]sPattern=getSPattern();
+        };
+
+        for (int i=0; i<oPattern.length;i++) {
+			for (int i=0; i<pPattern.length;i++) {
+				for (int i=0; i<sPattern.length;i++) {
+        }
+    }
+}
